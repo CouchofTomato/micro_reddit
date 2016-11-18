02fa74507bc198681455b3c1d574c296392976a8
Austin Mason <austinpmason@gmail.com>
2016-11-18 13:21:31 +0000

    create post model

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6, maximum: 15 }
 end
